1a84971e3ebe5a728ebc3cf5ca84ddd124ec0311
PauloGoncalvesLima <paulogoncalves436@gmail.com>
2018-05-25 00:08:03 -0300

    Adicionado o item mochila e mudadando o nome de uma pasta.

--- Trabalho Unity/Assets/Scripts/Player/PlayerInventory.cs
+++ Trabalho Unity/Assets/Scripts/Player/PlayerInventory.cs
@@ -1,11 +1,12 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerInventory : MonoBehaviour {
 
     public GameObject[] inventory = new GameObject[4];
-    public bool itemAdd;
+    public bool itemAdd,mochilacheck = false;
     public void AddItem(GameObject item)
     {
         itemAdd = false;
@@ -14,16 +15,39 @@ public class PlayerInventory : MonoBehaviour {
         {
             if (inventory[i] == null)
             {
+                if(item.name == "mochila")
+                {
+                    mochilacheck = true;
+                    Array.Resize(ref inventory, 8);
+                    itemAdd = true;
+                    item.SendMessage("DoInterction");
+                    Debug.Log(item.name + "foi adicionado assim aumentando o tamanho da capacidade para 8.");
+                    break;
+                }
                 inventory[i] = item;
                 itemAdd = true;
                 item.SendMessage("DoInterction");
-                Debug.Log(item.name + "foi adicionado");
+                Debug.Log (item.name + "foi adicionado");
                 break;
             }
         }
+
+
         if (!itemAdd)
         {
-            Debug.Log("Inventrio Cheio - Item não adicionado.");
+            if (item.name == "mochila")
+            {
+                mochilacheck = true;
+                Array.Resize(ref inventory, 8);
+                itemAdd = true;
+                item.SendMessage("DoInterction");
+                Debug.Log(item.name + "foi adicionado assim aumentando o tamanho da capacidade para 8.");
+            }
+            else
+            {
+                Debug.Log("Inventrio Cheio - Item não adicionado.");
+            }
+            
         }
 
     }
